c1326ba8704387398735b984356409751fddf3d4
Charly Alizadeh <alizadehcharly@gmail.com>
2024-02-09 19:41:36 +0100

    test: add test for the graph -> DOT export

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
 using PowerModelsDataset
 using Test
+using Graphs
 
 @testset "PowerModelsDataset.jl" begin
-    # Write your tests here.
+    @testset "exporting graph to the dot format" begin
+        graph = SimpleGraph([0 1 0 0 1; 1 0 1 0 1; 0 1 0 0 0; 0 0 0 0 1; 1 1 0 1 0])
+        PowerModelsDataset._export_graph(graph, "temp.gv")
+        file_str = read(open("temp.gv", "r"), String)
+        @test file_str == """graph {
+    1 -- 2;
+    1 -- 5;
+    2 -- 3;
+    2 -- 5;
+    4 -- 5;
+}"""
+    rm("temp.gv")
+    end
 end
