ad66be687a55c2f4d757fd61a0219144da0ebb3c
Charly Alizadeh <alizadehcharly@gmail.com>
2024-02-13 14:44:03 +0100

    feat: add import graph from dot file

--- src/export_graph.jl
+++ src/export_graph.jl
@@ -10,3 +10,13 @@ function _export_graph(graph::SimpleGraph, path::AbstractString, to::DotExportTy
     write(io, "}")
     close(io)
 end
+
+function _import_graph(path::AbstractString, from::DotExportType=DotExportType())
+    lines = split(read(open(path, "r"), String), '\n')
+    graph = SimpleGraph(length(lines) - 2)
+    for l in lines[2:end-1]
+        src, dst = split(replace(l, " " => "", ";" => ""), "--")
+        add_edge!(graph, parse(Int, src), parse(Int, dst))
+    end
+    return graph
+end
