16097427bbb09400ea4785ca225421c45654901f
Charly Alizadeh <alizadehcharly@gmail.com>
2024-02-17 15:46:01 +0100

    test: update tests

--- test/decompose.jl
+++ test/decompose.jl
@@ -3,5 +3,8 @@
         insert_instance!(db, "data/case14.m")
         instance = SDPowerModelsDataset.select_instance(db, "case14", "0") |> DataFrame
         generate_decomposition!(db, instance[1, :], PowerModels.CholeskyExtension())
+        decomposition = SDPowerModelsDataset.select_decomposition_all(db) |> DataFrame
+        decomposition = decomposition[1, :]
+        solve_decomposition!(db, decomposition)
         rm("test_pm_db.sqlite")
 end
